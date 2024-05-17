c9b872c0fd7352337cb2f17d4a748ffdbe64411b
Katy DeCorah <katydecorah@gmail.com>
2024-05-16 21:02:09 -0400

    Update

--- test/end-to-end.test.js
+++ test/end-to-end.test.js
@@ -10,42 +10,48 @@ describe("End to end", () => {
     isbn = null;
   });
 
-  it.each([
-    { name: "default", providers: [] },
-    { name: "openlibrary", providers: ["openlibrary"] },
-    { name: "google", providers: ["google"] },
-  ])(
-    "%s Annihilation",
-    async ({ providers }) => {
-      isbn.provider(providers);
-      await expect(isbn.resolve("9780374104092")).resolves.toMatchSnapshot();
-    },
-    10_000,
-  );
+  describe("Annihilation", () => {
+    it.each([
+      { name: "default", providers: [] },
+      { name: "openlibrary", providers: ["openlibrary"] },
+      { name: "google", providers: ["google"] },
+    ])(
+      "%s",
+      async ({ providers }) => {
+        isbn.provider(providers);
+        await expect(isbn.resolve("9780374104092")).resolves.toMatchSnapshot();
+      },
+      10_000,
+    );
+  });
 
-  it.each([
-    { name: "default", providers: [] },
-    { name: "openlibrary", providers: ["openlibrary"] },
-    { name: "google", providers: ["google"] },
-  ])(
-    "%s Transcendent Kingdom",
-    async ({ providers }) => {
-      isbn.provider(providers);
-      await expect(isbn.resolve("9780385695176")).resolves.toMatchSnapshot();
-    },
-    10_000,
-  );
+  describe("Transcendent Kingdom", () => {
+    it.each([
+      { name: "default", providers: [] },
+      { name: "openlibrary", providers: ["openlibrary"] },
+      { name: "google", providers: ["google"] },
+    ])(
+      "%s",
+      async ({ providers }) => {
+        isbn.provider(providers);
+        await expect(isbn.resolve("9780385695176")).resolves.toMatchSnapshot();
+      },
+      10_000,
+    );
+  });
 
-  it.each([
-    { name: "default", providers: [] },
-    { name: "openlibrary", providers: ["openlibrary"] },
-    { name: "google", providers: ["google"] },
-  ])(
-    "%s Fantastic Mr. Fox",
-    async ({ providers }) => {
-      isbn.provider(providers);
-      await expect(isbn.resolve("9780140328721")).resolves.toMatchSnapshot();
-    },
-    10_000,
-  );
+  describe("Fantastic Mr. Fox", () => {
+    it.each([
+      { name: "default", providers: [] },
+      { name: "openlibrary", providers: ["openlibrary"] },
+      { name: "google", providers: ["google"] },
+    ])(
+      "%s",
+      async ({ providers }) => {
+        isbn.provider(providers);
+        await expect(isbn.resolve("9780140328721")).resolves.toMatchSnapshot();
+      },
+      10_000,
+    );
+  });
 });
