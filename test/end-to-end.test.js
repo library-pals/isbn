81ebaef2ade30a951de78f0b16d906667b8486ea
Katy DeCorah <katydecorah@gmail.com>
2025-05-04 15:22:32 -0400

    Update dependencies

--- test/end-to-end.test.js
+++ test/end-to-end.test.js
@@ -15,48 +15,33 @@ describe("End to end", () => {
       { name: "default", providers: [] },
       { name: "openlibrary", providers: ["openlibrary"] },
       { name: "google", providers: ["google"] },
-    ])(
-      "%s",
-      async ({ providers }) => {
-        isbn.provider(providers);
-        await expect(isbn.resolve("9780374104092")).resolves.toMatchSnapshot();
-      },
-      10_000,
-    );
-  });
+    ])("%s", async ({ providers }) => {
+      isbn.provider(providers);
+      await expect(isbn.resolve("9780374104092")).resolves.toMatchSnapshot();
+    });
+  }, 10_000);
 
   describe("Transcendent Kingdom", () => {
     it.each([
       { name: "default", providers: [] },
       { name: "openlibrary", providers: ["openlibrary"] },
       { name: "google", providers: ["google"] },
-    ])(
-      "%s",
-      async ({ providers }) => {
-        isbn.provider(providers);
-        await expect(isbn.resolve("9780385695176")).resolves.toMatchSnapshot();
-      },
-      10_000,
-    );
-  });
+    ])("%s", async ({ providers }) => {
+      isbn.provider(providers);
+      await expect(isbn.resolve("9780385695176")).resolves.toMatchSnapshot();
+    });
+  }, 10_000);
 
-  // eslint-disable-next-line jest/no-commented-out-tests
-  /*
   describe("Fantastic Mr. Fox", () => {
     it.each([
       { name: "default", providers: [] },
       { name: "openlibrary", providers: ["openlibrary"] },
       { name: "google", providers: ["google"] },
-    ])(
-      "%s",
-      async ({ providers }) => {
-        isbn.provider(providers);
-        await expect(isbn.resolve("9780142423431")).resolves.toMatchSnapshot();
-      },
-      10_000,
-    );
-  });
-  */
+    ])("%s", async ({ providers }) => {
+      isbn.provider(providers);
+      await expect(isbn.resolve("9780142423431")).resolves.toMatchSnapshot();
+    });
+  }, 10_000);
 
   describe("The Ministry of Time (audiobook)", () => {
     it.each([{ name: "default", providers: ["librofm"] }])(
