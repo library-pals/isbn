58caf3682998b9e45736b1f4c7e7597555aa06e6
Katy DeCorah <katydecorah@gmail.com>
2025-07-10 18:32:15 -0400

    Update end-to-end.test.js

--- test/end-to-end.test.js
+++ test/end-to-end.test.js
@@ -58,35 +58,26 @@ describe("End to end", () => {
   describe("The Ministry of Time (audiobook)", () => {
     const browserHeaders = {
       "User-Agent":
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
       Accept:
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate",
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+      "Accept-Language": "en-US,en;q=0.9",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Cache-Control": "no-cache",
       Connection: "keep-alive",
       "Upgrade-Insecure-Requests": "1",
+      "Sec-Fetch-Dest": "document",
+      "Sec-Fetch-Mode": "navigate",
+      "Sec-Fetch-Site": "none",
+      "Sec-Fetch-User": "?1",
     };
     it.each([{ name: "default", providers: ["librofm"] }])(
       "%s",
       async ({ providers }) => {
         isbn.provider(providers);
-        try {
-          const result = await isbn.resolve("9781797176888", {
-            headers: browserHeaders,
-          });
-          expect(result).toMatchSnapshot();
-        } catch (error) {
-          // Skip test if it fails in CI environment (GitHub Actions)
-          // This can happen when libro.fm returns different response codes (202) or blocks requests
-          if (process.env.CI || process.env.GITHUB_ACTIONS) {
-            console.log(
-              "Skipping librofm test in CI environment due to:",
-              error.message,
-            );
-            return;
-          }
-          throw error;
-        }
+        await expect(
+          isbn.resolve("9781797176888", { headers: browserHeaders }),
+        ).resolves.toMatchSnapshot();
       },
       15_000,
     );
